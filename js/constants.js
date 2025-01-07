f9eb45f85227a13edab6f11a13329135b8ea433c
AAhmad404 <aahmad01000001@gmail.com>
2025-01-07 02:37:27 -0500

    Initial commit

---
+++ js/constants.js
@@ -0,0 +1,49 @@
+/*
+* Constants
+*/
+
+// Board constants
+const EMPTY = '';
+
+const MAX_ROW = 3;
+const MAX_COLUMN = 3;
+
+const ONE = [0, 0];
+const TWO = [0, 1];
+const THREE = [0, 2];
+const FOUR = [1, 0];
+const FIVE = [1, 1];
+const SIX = [1, 2];
+const SEVEN = [2, 0];
+const EIGHT = [2, 1];
+const NINE = [2, 2];
+
+
+// Player constants
+const X = 'X';
+const O = 'O';
+
+
+// Bot levels
+OFF = 'Bot Off';
+EASY = 'Easy';
+MEDIUM = 'Medium';
+UNDEFEATED = 'Undefeated';
+
+
+// Player Selection
+RANDOM = 'Random'
+
+
+// Map contianing the name of the HTML squares.
+const numToSquare = {
+    1: "#game-square-one",
+    2: "#game-square-two",
+    3: "#game-square-three",
+    4: "#game-square-four",
+    5: "#game-square-five",
+    6: "#game-square-six",
+    7: "#game-square-seven",
+    8: "#game-square-eight",
+    9: "#game-square-nine"
+};
